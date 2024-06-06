baebaa044c156f69a791483aa5e805b6a56b0fbb
prem <C-PVenugopal@dctabudhabi.ae>
2024-06-06 15:47:45 +0530

    cc

--- src/main/java/com/example/dubaipolica/Demo1Application.java
+++ src/main/java/com/example/dubaipolica/Demo1Application.java
@@ -15,7 +15,7 @@ import com.example.dubaipolica.service.ElkService;
 public class Demo1Application {
 
 	public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+		SpringApplication.run(Demo1Application.class, args);
+	}
 
 }
